68a90229b5bffede774900f4928bd388051fcb3f
Nataniel <natanielarg@gmail.com>
2022-06-06 01:16:45 -0300

    Agregadas las dependencias necesarias al archivo pom.xml

--- TP5_Grupo16/src/main/java/ar/edu/unju/fi/tp5/model/Docente.java
+++ TP5_Grupo16/src/main/java/ar/edu/unju/fi/tp5/model/Docente.java
@@ -1,5 +1,11 @@
 package ar.edu.unju.fi.tp5.model;
 
+import org.springframework.stereotype.Component;
+
+/*
+ * Objeto Bind que Spring podra gestionar.
+ */
+@Component
 public class Docente {
 
 }
